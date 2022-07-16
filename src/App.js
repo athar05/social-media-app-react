43a5ee97958a266399dbbd0221c5fd003e0cbb2b
athar05 <82315023+athar05@users.noreply.github.com>
2022-07-16 16:27:36 +0400

    Updated App & Index js

--- src/App.js
+++ src/App.js
@@ -1,42 +1,17 @@
 import "./App.css";
+import {Fragment} from 'react';
+import PageRoutes from "./page-routes/PageRoutes";
+import {Link, BrowserRouter as Router} from "react-router-dom"
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img  alt="mockBee logo" width="180" height="180" />
-        <h1 className="brand-title">
-          Welcome to <span>mockBee!</span>
-        </h1>
-        <p className="brand-description">
-          Get started by editing <code>src/App.js</code>
-        </p>
-        <div className="links">
-          <a
-            href="https://mockbee.netlify.app/"
-            target="_blank"
-            rel="noreferrer"
-          >
-            Explore mockBee
-          </a>
-          <a
-            href="https://mockbee.netlify.app/docs/api/introduction"
-            target="_blank"
-            rel="noreferrer"
-          >
-            API Documentation
-          </a>
-          <a
-            href="https://github.com/neogcamp/mockBee"
-            target="_blank"
-            rel="noreferrer"
-          >
-            Contribute
-          </a>
-        </div>
-      </header>
-    </div>
-  );
-}
+
+const App=() => 
+<Router>
+ <Fragment>
+  <PageRoutes/>
+  <div> 
+  <Link to="/bookmark"> Hello</Link>
+  </div>
+  </Fragment>
+ </Router>
 
 export default App;
