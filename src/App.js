aa69e8052e34e3988ff9e947589c5366fd6fbe23
athar05 <82315023+athar05@users.noreply.github.com>
2022-07-16 16:17:08 +0400

    Code cleanup

--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
 import "./App.css";
-import logo from "./logo.png";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} alt="mockBee logo" width="180" height="180" />
+        <img  alt="mockBee logo" width="180" height="180" />
         <h1 className="brand-title">
           Welcome to <span>mockBee!</span>
         </h1>
