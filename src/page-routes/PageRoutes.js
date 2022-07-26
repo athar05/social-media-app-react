256ffc842d5f251f41e76dbfe4af456fa6ed7357
athar05 <82315023+athar05@users.noreply.github.com>
2022-07-26 15:31:27 +0400

    Added requires auth wrapper to different page routes

--- src/page-routes/PageRoutes.js
+++ src/page-routes/PageRoutes.js
@@ -8,18 +8,21 @@ import Login from "../pages/Login";
 import Signout from "../pages/Signout";
 import Signup from "../pages/Signup";
 import UserProfile from "../pages/UserProfile";
+import RequiresAuth from '../components/RequiresAuth';
+import { useSelector } from 'react-redux';
 
 const PageRoutes = () => {
+  const loginState = useSelector((state)=> state.auth.isAuthenticated)
   return (
     <Routes>
     <Route exact path="/" element={<LandingPage />}/>
-    <Route exact path="/home" element={<Home />}/>
-    <Route exact path="/explore" element={<Explore />}/>
-    <Route exact path="/bookmark" element={<Bookmark />}/>
+    <Route exact path="/home" element={<RequiresAuth login={loginState}><Home /></RequiresAuth>}/>
+    <Route exact path="/explore" element={<RequiresAuth login={loginState}><Explore /> </RequiresAuth>}/>
+    <Route exact path="/bookmark" element={<RequiresAuth login={loginState}><Bookmark /> </RequiresAuth>}/>
     <Route exact path="/login" element={<Login />}/>
     <Route exact path="/signup" element={<Signup />}/>
     <Route exact path="/signout" element={<Signout />}/>
-    <Route exact path="/profile" element={<UserProfile />}/>
+    <Route exact path="/profile" element={<RequiresAuth login={loginState}><UserProfile /> </RequiresAuth>}/>
     </Routes>
   )
 }
