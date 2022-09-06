7990cd501c472c6d8fb5c402b897085f35821a1b
athar05 <atharshakeel96@gmail.com>
2022-09-06 11:05:50 +0530

    updated forms

--- src/components/SigninForm.js
+++ src/components/SigninForm.js
@@ -1,177 +1,215 @@
-import React, {useRef, useState} from 'react';
-import { Avatar, Button, Grid, Paper, TextField, Typography, Box, FormControl, InputLabel, InputAdornment, IconButton, Input } from '@mui/material';
-import Visibility from '@mui/icons-material/Visibility';
-import VisibilityOff from '@mui/icons-material/VisibilityOff';
-import { Link } from 'react-router-dom';
-import axios from 'axios';
-import {signIn} from "../features/slices/authSlice"
-import { setAlert, removeAlert } from '../features/slices/alertSlice';
-import { useDispatch } from 'react-redux';
-import { nanoid } from '@reduxjs/toolkit';
-  import Alerts from './Alerts';
-import { useNavigate } from "react-router-dom"; 
-import LoginIcon from '@mui/icons-material/Login';
+import React, { useRef, useState } from "react";
+import {
+  Avatar,
+  Button,
+  Grid,
+  Paper,
+  TextField,
+  Typography,
+  Box,
+  FormControl,
+  InputLabel,
+  InputAdornment,
+  IconButton,
+  Input,
+} from "@mui/material";
+import Visibility from "@mui/icons-material/Visibility";
+import VisibilityOff from "@mui/icons-material/VisibilityOff";
+import { Link } from "react-router-dom";
+import axios from "axios";
+import { signIn } from "../features/slices/authSlice";
+import { setAlert, removeAlert } from "../features/slices/alertSlice";
+import { useDispatch } from "react-redux";
+import { nanoid } from "@reduxjs/toolkit";
+import Alerts from "./Alerts";
+import { useNavigate } from "react-router-dom";
+import LoginIcon from "@mui/icons-material/Login";
 
 const SigninForm = () => {
-    const id = nanoid()
+  const id = nanoid();
 
-    const dispatch =useDispatch();
-    const navigate = useNavigate()
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
 
-    const usernameInputRef = useRef();
-    const passwordInputRef = useRef();
+  const usernameInputRef = useRef();
+  const passwordInputRef = useRef();
 
-    //show/hide password functionality
+  //show/hide password functionality
 
-    const [showPassword, setShowPassword] = useState(false)
+  const [showPassword, setShowPassword] = useState(false);
 
-    const showPassowordHandler = () => {
-      setShowPassword((prev)=> !prev)
-    }
+  const showPassowordHandler = () => {
+    setShowPassword((prev) => !prev);
+  };
 
-    //function to send a sign in request to a user
+  //function to send a sign in request to a user
 
-    const signInUser = async (userInfo) => {
-      const {data}= await axios.post("api/auth/login", userInfo);
-      // console.log(data)
-      const {foundUser, encodedToken} = await data;
-      // console.log(foundUser)
-      localStorage.setItem('user', JSON.stringify(foundUser));
-      localStorage.setItem('auth_token', JSON.stringify(encodedToken))
-      return { user: foundUser, token: encodedToken };
-    }
+  const signInUser = async (userInfo) => {
+    const { data } = await axios.post("api/auth/login", userInfo);
+    // console.log(data)
+    const { foundUser, encodedToken } = await data;
+    // console.log(foundUser)
+    localStorage.setItem("user", JSON.stringify(foundUser));
+    localStorage.setItem("auth_token", JSON.stringify(encodedToken));
+    return { user: foundUser, token: encodedToken };
+  };
 
-    //function to send a guest sign in request to a server
-    const guestLoginHandler = (e) => {
-        e.preventDefault();
+  //function to send a guest sign in request to a server
+  const guestLoginHandler = (e) => {
+    e.preventDefault();
 
-        const userInfo = {
-            username: "adarshbalika",
-            password: "adarshBalika123"
-        }
-        signInUser(userInfo)
-        .then(({user, token})=> dispatch(signIn({user, token})))
-        .then(()=> dispatch(setAlert("Sign Up Successful", "success", id)))
-        .then(()=> setTimeout(()=> dispatch(removeAlert(id))))
-        .then(()=> 	navigate("/home", { replace: true }))
-        .catch((e)=> {
-          if (e.response.status=== 422) {
-            dispatch(setAlert("Username Already Exists", "error", id))
-            setTimeout(()=> dispatch(removeAlert(id)), 5000)
-          } else {
-            dispatch(setAlert(e.response.status, "error", id))
-            setTimeout(()=> dispatch(removeAlert(id)), 5000)
-          }
-        })
-    }
-
-    //function when user submits the form
-    const submitHandler = (e) => {
-        e.preventDefault();
-
-        const enteredUsername = usernameInputRef.current.value; 
-        const enteredPassword = passwordInputRef.current.value; 
-    
-
-        //form validation 
-
-        const userInfo = {
-            username: enteredUsername,
-            password: enteredPassword
-        }
-
-
-        if (!enteredUsername || enteredUsername.length <= 4 ) {
-            dispatch(setAlert("Enter A Valid Username", "error", id))
-            setTimeout(() => {
-                dispatch(removeAlert(id))
-            }, 5000);
-        } else if (enteredPassword.length < 6) {
-            dispatch(setAlert("Please Enter A Valid Password", "error", id))
-            setTimeout(() => {
-                dispatch(removeAlert(id))
-            }, 5000);   
+    const userInfo = {
+      username: "adarshbalika",
+      password: "adarshBalika123",
+    };
+    signInUser(userInfo)
+      .then(({ user, token }) => dispatch(signIn({ user, token })))
+      .then(() => dispatch(setAlert("Sign Up Successful", "success", id)))
+      .then(() => setTimeout(() => dispatch(removeAlert(id))))
+      .then(() => navigate("/home", { replace: true }))
+      .catch((e) => {
+        if (e.response.status === 422) {
+          dispatch(setAlert("Username Already Exists", "error", id));
+          setTimeout(() => dispatch(removeAlert(id)), 5000);
         } else {
-            signInUser(userInfo)
-            .then(({user, token})=> dispatch(signIn({user,token})))
-            .then(()=> dispatch(setAlert("Sign Up Successful", "success", id)))
-            .then(()=> setTimeout(()=> dispatch(removeAlert(id)), 2000))
-            .then(()=> 	navigate("/home", { replace: true }))
-            .catch((e)=> {
-          if (e.response.status === 422) {
-            dispatch(setAlert("Username Already Exists", "error", id))
-            setTimeout(()=> dispatch(removeAlert(id)), 5000)
-          } else {
-            dispatch(setAlert(e.response.status, "error", id))
-            setTimeout(()=> dispatch(removeAlert(id)), 5000)
-          }
-        })
+          dispatch(setAlert(e.response.status, "error", id));
+          setTimeout(() => dispatch(removeAlert(id)), 5000);
         }
+      });
+  };
 
-        clearInputFields()
-}
+  //function when user submits the form
+  const submitHandler = (e) => {
+    e.preventDefault();
 
-    //function to clear input fields after the form is submitted
+    const enteredUsername = usernameInputRef.current.value;
+    const enteredPassword = passwordInputRef.current.value;
 
-const clearInputFields = () => {
-  usernameInputRef.current.value = "";
-  passwordInputRef.current.value="";
-}
+    //form validation
+
+    const userInfo = {
+      username: enteredUsername,
+      password: enteredPassword,
+    };
+
+    if (!enteredUsername || enteredUsername.length <= 4) {
+      dispatch(setAlert("Enter A Valid Username", "error", id));
+      setTimeout(() => {
+        dispatch(removeAlert(id));
+      }, 5000);
+    } else if (enteredPassword.length < 6) {
+      dispatch(setAlert("Please Enter A Valid Password", "error", id));
+      setTimeout(() => {
+        dispatch(removeAlert(id));
+      }, 5000);
+    } else {
+      signInUser(userInfo)
+        .then(({ user, token }) => dispatch(signIn({ user, token })))
+        .then(() => dispatch(setAlert("Sign Up Successful", "success", id)))
+        .then(() => setTimeout(() => dispatch(removeAlert(id)), 2000))
+        .then(() => navigate("/home", { replace: true }))
+        .catch((e) => {
+          if (e.response.status === 422) {
+            dispatch(setAlert("Username Already Exists", "error", id));
+            setTimeout(() => dispatch(removeAlert(id)), 5000);
+          } else {
+            dispatch(setAlert(e.response.status, "error", id));
+            setTimeout(() => dispatch(removeAlert(id)), 5000);
+          }
+        });
+    }
+
+    clearInputFields();
+  };
+
+  //function to clear input fields after the form is submitted
+
+  const clearInputFields = () => {
+    usernameInputRef.current.value = "";
+    passwordInputRef.current.value = "";
+  };
 
   return (
-    <section className='sign-in'>
-    <Grid>
-      <Paper elevation={5} className="form-wrapper text-center text-primary">
-        <Grid align="center">
-          <Avatar className='form-avatar'>
-           <LoginIcon/>
-          </Avatar>
-          <h2>Sign In</h2>
-          <Typography className='py-1 text-dark'>Please Fill This Form To Sign In </Typography>
-        </Grid>
-        <form onSubmit={submitHandler}>
-          <section>
-            <Alerts />
-          </section>
-          <FormControl fullWidth sx={{ m: 1}}>
-          <TextField fullWidth name='username 'label='username' variant="standard" type="text" required={true} inputRef={usernameInputRef}/>
-          </FormControl>
-          <FormControl required={true} variant='standard' fullWidth sx={{ m: 1}}>
-          <InputLabel htmlFor="standard-adornment-password">Password</InputLabel>
-          <Input
-          fullWidth
-          name='password'
-          variant='standard'
-            type={showPassword ? 'text' : 'password'}
-            inputRef={passwordInputRef}
-            endAdornment={
-              <InputAdornment position="end">
-                <IconButton
-                  aria-label="toggle password visibility"
-                  onClick={showPassowordHandler}
-                  edge="end"
+    <section className="sign-in">
+      <Grid>
+        <Paper elevation={5} className="form-wrapper text-center text-primary">
+          <Grid align="center">
+            <Avatar className="form-avatar">
+              <LoginIcon />
+            </Avatar>
+            <h2>Sign In</h2>
+            <Typography className="py-1 text-dark">
+              Please Fill This Form To Sign In{" "}
+            </Typography>
+          </Grid>
+          <form onSubmit={submitHandler}>
+            <section>
+              <Alerts />
+            </section>
+            <FormControl fullWidth sx={{ m: 1 }}>
+              <TextField
+                fullWidth
+                name="username "
+                label="username"
+                variant="standard"
+                type="text"
+                required={true}
+                inputRef={usernameInputRef}
+              />
+            </FormControl>
+            <FormControl
+              required={true}
+              variant="standard"
+              fullWidth
+              sx={{ m: 1 }}
+            >
+              <InputLabel htmlFor="standard-adornment-password">
+                Password
+              </InputLabel>
+              <Input
+                fullWidth
+                name="password"
+                variant="standard"
+                type={showPassword ? "text" : "password"}
+                inputRef={passwordInputRef}
+                endAdornment={
+                  <InputAdornment position="end">
+                    <IconButton
+                      aria-label="toggle password visibility"
+                      onClick={showPassowordHandler}
+                      edge="end"
+                    >
+                      {showPassword ? <VisibilityOff /> : <Visibility />}
+                    </IconButton>
+                  </InputAdornment>
+                }
+                label="Password"
+              />
+            </FormControl>
+            <div className="flex-row-justified">
+              <Box m={1} pt={2}>
+                <Button type="submit" variant="contained" className="button">
+                  Sign In
+                </Button>
+              </Box>
+              <Box m={1} pt={2}>
+                <Button
+                  variant="contained"
+                  className="button"
+                  onClick={guestLoginHandler}
                 >
-                  {showPassword ? <VisibilityOff /> : <Visibility />}
-                </IconButton>
-              </InputAdornment>
-            }
-            label="Password"
-          />
-          </FormControl>
-          <div className='flex-row-justified'>
-          <Box m={1} pt={2}>
-          <Button type='submit' variant='contained' className='button' >Sign In</Button>
-          </Box>
-          <Box m={1} pt={2}>
-          <Button variant='contained' className='button' onClick={guestLoginHandler} >Guest Login</Button>
-          </Box>
-          </div>
-          <p className="my-1">Don't have an account? <Link to="/signup">Register Now</Link></p>
-        </form>
-      </Paper>
-    </Grid>
+                  Guest Login
+                </Button>
+              </Box>
+            </div>
+            <p className="my-1">
+              Don't have an account? <Link to="/signup">Register Now</Link>
+            </p>
+          </form>
+        </Paper>
+      </Grid>
     </section>
-  )
-}
+  );
+};
 
-export default SigninForm
+export default SigninForm;
